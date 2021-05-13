209ed9af796b124a47c7112b7bf23837ca946a71
ТatanaNedosekina <57523148+Tatana3@users.noreply.github.com>
2021-05-13 13:04:11 +0300

    commit

--- Таня/labGame/labGame/StartVirus.cs
+++ Таня/labGame/labGame/StartVirus.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace labGame
+    
+    //запуск стартовой формы игры VirusGame
 {
     public partial class StartVirus : Form
     {
@@ -18,12 +20,13 @@ namespace labGame
         private void button1_Click(object sender, EventArgs e)
         {
             start_VirusGame();
-        }
+        } //кнопка старта игры
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
+        }//кнопка закрытия формы
+        
         private void start_VirusGame()
         {
             VirusGame VG = new VirusGame();
